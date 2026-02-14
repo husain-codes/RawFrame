7de9e74a68755110a7578cdc660408d659a9cdd1
Husain Khan <hk149335@gmail.com>
2026-02-14 07:34:05 +0000

    feat: img structure defination

--- include/img/img.h
+++ include/img/img.h
@@ -2,12 +2,7 @@
 #define IMG_H
 
 #include <stdint.h>
-/* Image pixel formats */
-typedef enum {
-    IMG_FMT_GRAY8,
-    IMG_FMT_RGB24,
-    IMG_FMT_RGBA32
-}img_format_t;
+#include "img_format.h"
 
 /*Image structure */
 typedef struct {
---
+++ include/img/img_format.h
@@ -0,0 +1,12 @@
+#ifndef IMG_FORMAT_H
+#define IMG_FORMAT_H
+
+/* Image pixel formats */
+typedef enum {
+    IMG_FMT_UNKNOWN = 0,
+    IMG_FMT_GRAY8,
+    IMG_FMT_RGB24,
+    IMG_FMT_RGBA32
+}img_format_t;
+
+#endif
