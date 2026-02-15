d6bfbe902a15a7327c76142c285b64177cfb7bb9
Husain Khan <hk149335@gmail.com>
2026-02-15 17:16:57 +0000

    feat: load bmp image

---
+++ examples/image_copy.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <io/img_io.h>
+
+int main(int argc , char* argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <input.bmp>\n",argv[0]);
+	return -1;
+    }
+
+    img_t* image = img_load_bmp(argv[1]);
+    if(!image)
+    {
+        fprintf(stderr,"Failed to load image: %s\n", argv[1]);
+	return -1;
+    }
+    printf("created image with %d x %d \n",image->width, image->height);
+    int w = image->width;
+    int h = image->height;
+
+    // Sample points
+    int samples[3][2] = {
+        {0, 0},                // top-left
+        {w/2, h/2},            // center
+        {w-1, h-1}             // bottom-right
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+       int x = samples[i][0];
+       int y = samples[i][1];
+
+       unsigned char* pixel = image->data + (y * w + x) * 3;
+       unsigned char r = pixel[0];
+       unsigned char g = pixel[1];
+       unsigned char b = pixel[2];
+       printf("Pixel (%d,%d) -> R=%u G=%u B=%u\n",
+              x, y, r, g, b);
+    }
+
+/*    img_t* copy = img_copy(image);
+    if(!copy)
+    {
+	img_destroy(image);
+	image = NULL;
+        printf("Failed to copy image\n");	
+        return -1;
+    }
+
+    if (img_save_bmp("output.bmp",copy) != 0)
+    {
+        fprintf(stderr,"Failed to save output.bmp\n");
+    }
+    
+    printf("Image copied and saved to output.bmp\n");
+  */  // Clean up resources.
+    img_destroy(image);
+    image = NULL;
+    //img_destroy(copy);
+    //copy = NULL;
+    return 0;
+}
---
+++ include/io/img_io.h
@@ -0,0 +1,32 @@
+#ifndef IMG_IO_H
+#define IMG_IO_H
+#include <stdint.h>
+#include <img/img.h>
+
+#pragma pack(push,1)
+typedef struct {
+    uint16_t signature;
+    uint32_t file_size;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t data_offset;
+}BMHEADER;
+
+typedef struct {
+    uint32_t info_header_size;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t bits_per_pixel;
+    uint32_t compression;
+    uint32_t image_size;
+    uint32_t x_pixels_per_m;
+    uint32_t y_pixels_per_m;
+    uint32_t color_used;
+    uint32_t imp_colors;
+}BMINFOHEADER;
+#pragma pack(pop)
+
+img_t* img_load_bmp(char* path);
+img_t* img_copy(img_t* img);
+#endif
