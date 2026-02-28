4b3188a65b0b68d70f5cb7a58518fdb6ac88e279
Husain Khan <hk149335@gmail.com>
2026-02-28 18:09:33 +0000

    feat: add image copy logic

--- examples/image_copy.c
+++ examples/image_copy.c
@@ -34,22 +34,32 @@ int main(int argc, char *argv[]) {
     printf("Pixel (%d,%d) -> R=%u G=%u B=%u\n", x, y, r, g, b);
   }
 
-  /*    img_t* copy = img_copy(image);
-      if(!copy)
-      {
-          img_destroy(image);
-          image = NULL;
-          printf("Failed to copy image\n");
-          return -1;
-      }
+  img_t *copy = img_copy(image);
+  if (!copy) {
+    img_destroy(image);
+    image = NULL;
+    printf("Failed to copy image\n");
+    return -1;
+  }
+  int wc = copy->width;
+  int hc = copy->height;
+  for (int i = 0; i < 3; i++) {
+    int x = samples[i][0];
+    int y = samples[i][1];
 
-      if (img_save_bmp("output.bmp",copy) != 0)
-      {
-          fprintf(stderr,"Failed to save output.bmp\n");
-      }
-
-      printf("Image copied and saved to output.bmp\n");
-    */  // Clean up resources.
+    unsigned char *pixel = copy->data + (y * wc + x) * 3;
+    unsigned char r = pixel[0];
+    unsigned char g = pixel[1];
+    unsigned char b = pixel[2];
+    printf("COPY: Pixel (%d,%d) -> R=%u G=%u B=%u\n", x, y, r, g, b);
+  }
+  /*     if (img_save_bmp("output.bmp",copy) != 0)
+       {
+           fprintf(stderr,"Failed to save output.bmp\n");
+       }
+ */
+  printf("Image copied and saved to output.bmp\n");
+  // Clean up resources.
   img_destroy(image);
   image = NULL;
   // img_destroy(copy);
